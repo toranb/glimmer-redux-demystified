6df20ec40930f7c90ccfb3aff4da57bc942dc1d2
toran billups <toranb@gmail.com>
2017-11-11 15:38:23 -0600

    glimmer redux in action

--- src/ui/components/Demo/component.ts
+++ src/ui/components/Demo/component.ts
@@ -1,10 +1,29 @@
 import Component, { tracked } from '@glimmer/component';
+import { createStore } from 'redux';
+
+const reducers = (state, action) => {
+  if(action.type === 'ADD') {
+    return state + 1;
+  }
+  return state || 0;
+};
+const store = createStore(reducers);
 
 export default class Demo extends Component {
-  @tracked num: number;
+  @tracked _num: number;
+  @tracked('_num')
+  get num() {
+    return store.getState();
+  }
+
+  constructor(args) {
+    store.subscribe(() => {
+      this._num = store.getState();
+    });
+    super(args);
+  }
 
   add() {
-    const value = this.num || 0;
-    this.num = value + 1;
+    store.dispatch({type: 'ADD'});
   }
 }
