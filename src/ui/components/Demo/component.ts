825f7090ecf9285e50d589bc445b2437169bce1a
toran billups <toranb@gmail.com>
2017-11-11 15:30:06 -0600

    hello world

--- src/ui/components/Demo/component.ts
+++ src/ui/components/Demo/component.ts
@@ -1,5 +1,10 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class Demo extends Component {
+  @tracked num: number;
 
+  add() {
+    const value = this.num || 0;
+    this.num = value + 1;
+  }
 }
